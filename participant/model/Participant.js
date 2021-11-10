f9f1e913361a70af3b1f5479a6985320ba9df097
sarthak rawat <90497870+sarthak-rawat-1101@users.noreply.github.com>
2021-11-10 15:47:55 +0530

    Updated Participant.js (schema)

--- participant/model/Participant.js
+++ participant/model/Participant.js
@@ -1,1 +1,16 @@
 // participant schema
+const { model } = require('mongoose');
+const mongoose = require('mongoose');
+
+const participant_schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Participant", participant_schema);
