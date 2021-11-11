bbd3ba33aafe0278069216bde004d6e1ec253aba
sarthak rawat <90497870+sarthak-rawat-1101@users.noreply.github.com>
2021-11-11 18:24:16 +0530

    Update Participant.js (Schema)

--- participant/model/Participant.js
+++ participant/model/Participant.js
@@ -3,6 +3,10 @@ const { model } = require('mongoose');
 const mongoose = require('mongoose');
 
 const participant_schema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
     name: {
         type: String,
         required: true
@@ -10,7 +14,21 @@ const participant_schema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    tests: [{
+        test_id: {
+            type: String,
+            required: true
+        }
+    }],
 });
 
 module.exports = mongoose.model("Participant", participant_schema);
