aaf9a6f0cd41dc6fbb668a93141070d9edc89fd6
PADAM PRAKASH <padamprakash2014@gmail.com>
2021-11-27 20:28:47 +0530

    recording_meged

---
+++ public/script_rec.js
@@ -0,0 +1,61 @@
+  
+  const start = document.getElementById("start");
+  const stop = document.getElementById("stop");
+  const video = document.querySelector("video");
+  let recorder, stream;
+
+  let count=0
+
+  function play_recording()
+  {
+      video.style.display="block";
+      video.play();
+  }
+  
+  function play_from_localstorage()
+  {
+    var url=localStorage.getItem("url");
+    const link = document.createElement('a');
+    //link.download = "recording.webm";
+    //link.click();
+    link.href = encodeURI(url);
+    window.open(link.href,'_blank');    
+  }
+  
+  async function startRecording() {
+    stream = await navigator.mediaDevices.getDisplayMedia({
+      video: { mediaSource: "screen" }
+    });
+    recorder = new MediaRecorder(stream);
+    
+    const chunks = [];
+    recorder.ondataavailable = e => chunks.push(e.data);
+    recorder.onstop = e => {
+      const completeBlob = new Blob(chunks, { type: chunks[0].type });
+      video.src = URL.createObjectURL(completeBlob);
+      const link = document.createElement('a');
+      link.href = video.src;
+      count = count + 1;
+      link.download = count.toString()+".webm";
+      link.click();
+      localStorage.setItem("url",link.href);
+    };
+   recorder.start();
+  }
+  
+  start.addEventListener("click", () => {
+    start.setAttribute("disabled", true);
+    stop.removeAttribute("disabled");
+    show.setAttribute("disabled",true);
+    startRecording();
+  });
+  
+  stop.addEventListener("click", () => {
+    stop.setAttribute("disabled", true);
+    start.removeAttribute("disabled");
+    show.removeAttribute("disabled");
+    
+    recorder.stop();
+    stream.getVideoTracks()[0].stop();
+  });
+  
